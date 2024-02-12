9c3518677883bef21f46a3440597ac0f67f95c67
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-12 11:12:23 +0200

    Add Gerenric Repository

---
+++ Mawgood/Mawgood.EF/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using Mawgood.Core.IRepositories;
+using Mawgood.Core.IResponses.IResponseMessage;
+using Mawgood.EF.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mawgood.EF.Repositories
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public Task<IEnumerable<IResponseMessage<T>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResponseMessage<T>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResponseMessage<T>> GetFirstAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IResponseMessage<T>>> GetWhereAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<IResponseMessage<T>> Add(T model)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IResponseMessage<T>> Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResponseMessage<T>> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- Mawgood/Mawgood.core/IRepositories/IGenericRepository.cs
+++ Mawgood/Mawgood.core/IRepositories/IGenericRepository.cs
@@ -11,17 +11,19 @@ namespace Mawgood.Core.IRepositories
     {
 
         // get methods
-        
-        Task<IResponseMessage<T>> GetByIdAsync(int id);
         Task<IEnumerable<IResponseMessage<T>>> GetAllAsync();
+        Task<IResponseMessage<T>> GetByIdAsync(int id);
         Task<IEnumerable<IResponseMessage<T>>> GetWhereAsync(Expression<Func<T, bool>> predicate);
         Task<IResponseMessage<T>> GetFirstAsync(Expression<Func<T, bool>> predicate);
 
         // add methods
+        Task<IResponseMessage<T>> Add(T model);
 
-        
+        // update methods
+        Task<IResponseMessage<T>> Update(T model);
 
-
+        // delete method
+        Task<IResponseMessage<T>> Delete(int id);
 
     }
 }
