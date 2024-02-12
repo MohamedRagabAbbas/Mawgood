40d78451899f4c562fe8c2c72cac8c91b8effc6f
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-12 12:47:52 +0200

    add the models' repository interfaces

---
+++ Mawgood/Mawgood.core/IRepositories/IAuthenticator.cs
@@ -0,0 +1,15 @@
+﻿using Mawgood.Core.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mawgood.Core.IRepositories
+{
+    public interface IAuthenticator:IGenericRepository<User>
+    {
+        
+    }
+}
---
+++ Mawgood/Mawgood.core/IRepositories/IJobSeekerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mawgood.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mawgood.Core.IRepositories
+{
+    internal interface IJobSeekerRepository:IGenericRepository<JobSeeker>
+    {
+    }
+}
