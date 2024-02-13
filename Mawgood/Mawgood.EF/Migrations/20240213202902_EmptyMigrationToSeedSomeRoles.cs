11cb7785568294c9756ebc9bbed902ed620977a9
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-13 22:32:20 +0200

    Add new migration in the sake of adding two role manually which are Employer and JobSeeker

---
+++ Mawgood/Mawgood.EF/Migrations/20240213202902_EmptyMigrationToSeedSomeRoles.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mawgood.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmptyMigrationToSeedSomeRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // seed some roles
+            migrationBuilder.Sql("INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('1', 'Employer', 'EMPLOYER')");
+            migrationBuilder.Sql("INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('2', 'JobSeeker', 'JOBSEEKER')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
