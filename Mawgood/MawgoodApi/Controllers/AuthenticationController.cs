20caf047be1cf33f9fb8d860d219a912354d2947
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-13 23:15:36 +0200

    add Authentication Contoller

---
+++ Mawgood/MawgoodApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using Mawgood.Core.DTO.Request;
+using Mawgood.EF.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MawgoodApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly AuthenticationService _authenticationService;
+        public AuthenticationController(AuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+        [Route("login/{email}/{password}")]
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var response = await _authenticationService.Authenticate(email,password);
+            return Ok(response);
+        }
+        [Route("register-jobseeker")]
+        [HttpPost]
+        public async Task<IActionResult> RegisterJobSeeker([FromBody] JobSeekerRegistrationRequest request)
+        {
+            var response = await _authenticationService.RegisterJobSeeker(request);
+            return Ok(response);
+        }
+        [Route("register-employer")]
+        [HttpPost]
+        public async Task<IActionResult> RegisterEmployer([FromBody] EmployerRegistrationRequest request)
+        {
+            var response = await _authenticationService.RegisterEmployer(request);
+            return Ok(response);
+        }
+    }
+}
